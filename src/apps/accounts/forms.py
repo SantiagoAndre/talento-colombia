c8ab68da0e2e5c726878e338e7c7edd781c0a9dd
santiago <saramirez@unicauca.edu.co>
2020-04-29 18:26:00 -0500

    Update create user, add constraint uniquie email, done HU-003

--- src/apps/accounts/forms.py
+++ src/apps/accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-
+from .models import User,AspiringUser,CompanyUser
 
 User = get_user_model()
 class UserCreationForm(forms.ModelForm):
@@ -20,17 +20,37 @@ class UserCreationForm(forms.ModelForm):
         # Check that the two password entries match
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(email=""):
+            raise forms.ValidationError("Constraint error,user already exists with blank email. Please update email of this user")
+        if User.objects.filter(username=username):
+            raise forms.ValidationError("A user with that username already exists.")
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
         return self.cleaned_data
 
     def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(UserCreationForm, self).save(commit=False)
+        #create user depends of user_type
+        user_type = self.cleaned_data.get("user_type")
+        username= self.cleaned_data.get("username")
+        is_superuser = False
+        if user_type == User.ADMIN:
+            Model = User
+            is_superuser = True
+        elif user_type == User.COMPANY:
+            Model = CompanyUser
+        else:
+            Model = AspiringUser
+        user = Model.objects.create(username=username,user_type=user_type,is_superuser=is_superuser)
         user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
+            instance.save_m2m()
+        else:
+            self.save_m2m = self._save_m2m
         return user
+    
+   
 
 
 
