24a1f4ee725578147b45fac0766c3e43bbd6e610
santiago <saramirez@unicauca.edu.co>
2020-04-30 18:11:04 -0500

    list company jobcalls done, HU-004-01

--- src/apps/company/urls.py
+++ src/apps/company/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
-from .views import CreateJobCallView
+from .views import CreateJobCallView,CompanyJobCallsView
 urlpatterns = [
     path('create_jobcall', CreateJobCallView.as_view(), name='company.create_jobcall'),
+    path('', CompanyJobCallsView.as_view(), name='company.jobcalls'),
 ]
