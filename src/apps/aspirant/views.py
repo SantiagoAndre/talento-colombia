ad42582f41572583a42fcfb9a5fef0d23ccdf75b
santiago <saramirez@unicauca.edu.co>
2020-04-30 20:36:35 -0500

    list aspirant jobcalls done done HU-005-01

--- src/apps/aspirant/views.py
+++ src/apps/aspirant/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import redirect
-from apps.jobcall.models import JobCall
+from django.shortcuts import redirect,render
 from apps.accounts.models import AspiringUser
+from django.views import View
+
+from apps.jobcall.models import JobCall
+
+
+
 
 # Create your views here.
 
@@ -17,4 +22,16 @@ def discard_jobcall(request,jobcall_id=None):
     user = AspiringUser.objects.get(pk=request.user.id)
     jobcall.aspirants.remove(user)
   return redirect('/')
-  
+
+class AllJobCallsView(View):
+  def get(self, request): 
+    context = {
+    'jobcalls': JobCall.objects.filter(aspirants__id=request.user.id).order_by('closing_date'),
+    }
+    return render(request, 'jobcall/list_jobcalls.html', context=context)
+
+  def post(self, request):
+    pass
+
+  def push(self, request):
+    pass
